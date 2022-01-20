4455041d0f303238c4f6b0f88e33f3d65f047538
BerndiVader <kasermandel@gmail.com>
2022-01-20 17:12:48 +0100

    added console runner for headless input

--- src/main/java/com/gmail/berndivader/biene/Headless.java
+++ src/main/java/com/gmail/berndivader/biene/Headless.java
@@ -1,18 +1,47 @@
 package com.gmail.berndivader.biene;
 
-import java.util.TimerTask;
+import java.io.Console;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
-public class Headless extends TimerTask {
+public class Headless {
+	
+	static Console console;
+	static Scanner keyboard;
+	
+	public static boolean exit;
+	
+	static {
+		console=System.console();
+		keyboard=new Scanner(console.reader());
+		exit=false;
+	}
 	
 	public Headless() {
 		
+		while(!exit) {
+			String input=console();
+			if(input!=null) {
+				if(input.startsWith(".")) {
+					
+				} else {
+					console.printf("%s",input);
+				}
+			}
+		}
 		
+		keyboard.close();
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	private String console() {
+		console.printf("%s",">");
+		String input;
+		try {
+	        input = keyboard.nextLine().toLowerCase();
+		} catch (NoSuchElementException e) {
+			input="";
+		}
+		return input;
 	}
 
 }
