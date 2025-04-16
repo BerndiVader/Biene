d351442daf48210f5da769eaaa46e19abcce7be4
BerndiVader <kasermandel@gmail.com>
2025-04-16 20:10:50 +0200

    feat: enhance category insertion logic in Wl2Import class for improved handling of existing categories

--- php/export/winline2_xtc_import.php
+++ php/export/winline2_xtc_import.php
@@ -27,6 +27,117 @@ class Wl2Import extends xtcImport
         return $mfn_array;
     }
 
+    private function getStoredCatID(array $tree,string $catTree) 
+    {
+        $keys=explode("']['",trim($catTree,"[]'"));
+        $current=$tree;
+    
+        foreach($keys as $key)
+        {
+            if(isset($current[$key]))
+            {
+                $current=$current[$key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    
+        return $current['ID']??null;
+    }
+    
+    function insertCategory(&$dataArray,$pID,$mode='insert') 
+    {
+        $cat=[];
+        $catTree="";
+        for($i=0;$i<$this->catDepth;$i++)
+        {
+            if(trim($dataArray["p_cat.{$i}"])!="")
+            {
+                $cat[$i]=xtc_db_prepare_input(trim($dataArray["p_cat.{$i}"]));
+                $catTree.='[\''.xtc_db_input($cat[$i]).'\']';
+            }
+        }
+
+        $ID=$this->getStoredCatID($this->CatTree,$catTree);
+
+        if(isset($ID)&&(is_int($ID)||$ID=='0'))
+        {
+            $this->insertPtoCconnection($pID,$ID);
+        }
+        else
+        {
+            $catTree="";
+            $parTree="";
+            $curr_ID=0;
+            for($i=0,$cc=count($cat);$i<$cc;$i++)
+            {
+                $catTree.='[\''.xtc_db_input($cat[$i]).'\']';
+                $ID=$this->getStoredCatID($this->CatTree,$catTree);
+
+                if(isset($ID)&&(is_int($ID)||$ID=='0'))
+                {
+                    $curr_ID=$ID;
+                }
+                else
+                {
+                    $parent=$this->getStoredCatID($this->CatTree,$parTree);
+                    $cat_query=xtc_db_query(
+                        "SELECT c.categories_id
+                        FROM ".TABLE_CATEGORIES." c
+                        JOIN ".TABLE_CATEGORIES_DESCRIPTION." cd
+                        ON cd.categories_id = c.categories_id
+                        WHERE cd.categories_name = '".xtc_db_input($cat[$i])."'
+                        AND cd.language_id = '".$this->languages[0]['id']."'
+                        AND c.parent_id = '".$parent."'"
+                    );
+                    
+                    if(!xtc_db_num_rows($cat_query))
+                    {
+                        $categorie_data=[
+                            'parent_id'=>$parent,
+                            'categories_status'=>1,
+                            'date_added'=>'now()',
+                            'last_modified'=>'now()'
+                        ];
+
+                        xtc_db_perform(TABLE_CATEGORIES,$categorie_data);
+                        $cat_id=xtc_db_insert_id();
+
+                        $this->counter['cat_new']++;
+
+                        $code = '$this->CatTree'.$parTree.'[\''.xtc_db_input($cat[$i]).'\'][\'ID\']='.$cat_id.';';
+                        eval ($code);
+
+                        $parent = $cat_id;
+                        for ($i_insert = 0; $i_insert < $this->sizeof_languages; $i_insert ++) {
+                            $categorie_data = array (
+                                'language_id' => $this->languages[$i_insert]['id'],
+                                'categories_id' => $cat_id,
+                                'categories_name' => $cat[$i]
+                            );
+                            xtc_db_perform(TABLE_CATEGORIES_DESCRIPTION, $categorie_data);
+                        }
+                    } else {
+                        $this->counter['cat_touched']++;
+                        $cData = xtc_db_fetch_array($cat_query);
+                        $cat_id = $cData['categories_id'];
+                        $code = '$this->CatTree'.$parTree.'[\''.xtc_db_input($cat[$i]).'\'][\'ID\']='.$cat_id.';';
+                        //debug
+                        if ($this->debug) {echo '<pre>FIFTH $CODE: ' . $code . '</pre>';}
+
+                        eval ($code);
+                        //debug
+                        if ($this->debug) echo '<pre>SECOND $CAT_ID: '.$cat_id.'</pre><hr />';
+                    }
+                }
+                $parTree = $catTree;
+            }
+            $this->insertPtoCconnection($pID, $cat_id);
+        }
+    }
+
 }
 
 class Wl2Export extends xtcExport
